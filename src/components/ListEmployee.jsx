50a659e1b66b123bfc22c075d76f89fe2494b85b
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-07-15 02:05:02 +0530

    rest api fetch with axios
    rest api generated with springboot

--- src/components/ListEmployee.jsx
+++ src/components/ListEmployee.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import EmployeeService from '../sevices/EmployeeService';
 
 export default function ListEmployee() {
     const [employee, setEmployee] = useState([]);
+    
+    useEffect(()=> {
+       EmployeeService.getEmployees().then((res)=>{
+           setEmployee(res.data);
+       });
+    }, []);
 
     return (
         <div>
-            <h2 className="text-center">Employees List</h2>
+            <h2 className="text-center px-lg-5 mt-lg-4">Employees List</h2>
             <div className="row">
-                <table className="table table-striped table-bordered">
+                <table className="table  table-bordered px-lg-5 mt-lg-4">
                     <thead>
                         <tr>
                             <th>Employee First Name</th>
@@ -19,13 +26,13 @@ export default function ListEmployee() {
                     </thead>
                     <tbody>
                         {
-                            employee.map(e => {
+                            employee.map((e) => (
                                 <tr key={e.id}>
                                     <td>{e.firstName}</td>
                                     <td>{e.lastName}</td>
                                     <td>{e.emailId}</td>
                                 </tr>
-                            })
+                            ))
                         }
 
                     </tbody>
