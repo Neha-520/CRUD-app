bb5d00054c3e7f94bea37538e0583323cb588253
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-07-16 00:58:50 +0530

    createEmployee component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import ListEmployee from './components/ListEmployee';
+
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Switch, Route,Router } from "react-router-dom";
+import { Switch, Route,BrowserRouter as Router} from "react-router-dom";
+import CreateEmployee  from './components/CreateEmployee';
 
 function App() {
   return (
    <div>
-   {/* <Router> */}
+<Router> 
       <Header/>
     <div className="container">
-      {/* <Switch> */}
-        {/* <Route exact  path="/" component={ListEmployee}></Route>
-        <Route exact  path="/employees" component={ListEmployee}></Route>
-        <Route exact  path="/" component={ListEmployee}></Route> */}
-        <ListEmployee/>
-      {/* </Switch> */}
+   <Switch> 
+        <Route exact  path="/" component={ListEmployee}></Route>
+        <Route  path="/employees" component={ListEmployee}></Route>
+        <Route  path="/add-employee" component={CreateEmployee}></Route>
+        <CreateEmployee/>
+   </Switch> 
      </div>
       <Footer/>
-      {/* </Router> */}
+   </Router> 
     </div>
   );
 }
--- src/components/CreateEmployee.jsx
+++ src/components/CreateEmployee.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 
-export const CreateEmployee = () => {
+export default function CreateEmployee (){
+const [firstName,setfirstName]=useState(" ");
+const [lastName,setlastName]=useState(" ");
+const [emailId,setemailId]=useState(" ");
+
+const FirstNameHandler=(event)=>{
+    setfirstName(event.target.value);
+}
+const LastNameHandler=(event)=>{
+    setlastName(event.target.value);
+}
+const EmailIdHandler=(event)=>{
+    setemailId(event.target.value);
+}
+const saveEmployee=(e)=>{
+e.preventDefault();
+}
+const Cancel=()=>{
+
+}
+
     return (
         <div>
-            
+          <div className="container">
+              <div className="row">
+                  <div className="card col-md-6 offset-md-3 mt-lg-5 ">
+                    <h3 className="text-center mt-3">Add Employee</h3>
+                     <div className="card-body">
+                        <form>
+                            <div className="form-group">
+                            <label>First Name: </label>
+                             <input placeholder="First Name" name="firstName" className="form-control" value={firstName} onChange={FirstNameHandler}/>
+                            </div>
+
+                            <div className="form-group">
+                            <label>Last Name: </label>
+                             <input placeholder="Last Name" name="lastName" className="form-control" value={lastName} onChange={LastNameHandler}/>
+                            </div>
+
+                            <div className="form-group">
+                            <label>Email Id: </label>
+                             <input placeholder="Email Address" type="email" name="emailId" className="form-control" value={emailId} onChange={EmailIdHandler}/>
+                            </div>
+
+                            <div className="mt-5 mb-3">
+                            <center>
+                            <button className="btn btn-success" onClick={saveEmployee}>Save</button>
+                            <button className="btn btn-danger" style={{marginLeft:"10px"}}onClick={Cancel}>Cancel</button>
+                            </center>
+                            </div>
+                        </form>
+                     </div>
+                  </div>
+
+              </div>
+          </div>   
         </div>
     )
 }
